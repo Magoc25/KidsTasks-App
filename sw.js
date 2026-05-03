faacdaf1fb028f3d3de40ccc4bb17836ef2b5a81
Magoc25 <129018821+Magoc25@users.noreply.github.com>
2026-05-03 11:25:26 -0300

    fix: adiciona listener do editGoalForm que estava faltando; SW v25

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 /* KidsTasks — Service Worker v1.0.0 */
 
-const CACHE_NAME = 'kidstasks-v24';
+const CACHE_NAME = 'kidstasks-v25';
 
 self.addEventListener('install', event => {
   self.skipWaiting();
